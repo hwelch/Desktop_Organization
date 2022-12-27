e9bcc3d8369d08f286524a6396a6c398824807c3
Hunter Welch <hjw36@case.edu>
2022-12-27 16:06:37 -0600

    Added Sorting Logic
    Properly sorts files depending on if they are text files or other files

--- File_Organization/sort_text_files.py
+++ File_Organization/sort_text_files.py
@@ -7,7 +7,7 @@ import os
 # read in the file directory we are sorting from the user
 parent_dir = input("Enter Directory you want to sort: ")
 
-# first we will make the two files to store everything if they are not already made
+# first we will make the two directories to store everything if the folders to not already exist
 os.chdir(parent_dir)
 files = os.listdir()
 
@@ -17,10 +17,35 @@ other_dir = 'other'
 text_path = os.path.join(parent_dir, text_files_dir)
 other_path = os.path.join(parent_dir, other_dir)
 
-if text_files_dir not in files and other_dir not in files:
+if text_files_dir not in files:
     os.mkdir(text_path)
-    os.mkdir(other_path)
-    print("Directories {text_path} and {other_path} added")
+    print(f"Directory {text_path} added")
 else:
-    print("Directories {text_path} and {other_path} already existed")
+    print(f"Directory {text_path} already existed")
 
+if other_dir not in files:
+    os.mkdir(other_path)
+    print(f"Directory {other_path} added")
+else:
+    print(f"Directory {other_path} already existed")
+
+
+# function to change the directory of a given file
+def change_dir(file, new_dir):
+    old_path = os.path.join(parent_dir, file)
+    new_path = os.path.join(new_dir, file)
+    os.rename(old_path, new_path)
+
+
+count = 0
+# next we will loop through all of the files in the parent directory 
+# and sort them into the two directories (text_files and other)
+for file in files:
+    if file != 'text_files' and file != 'other':
+        if '.pdf' in file or '.txt' in file or '.doc' in file:
+            change_dir(file, text_path)
+        else:
+            change_dir(file, other_path)
+        count += 1
+
+print(f'Organized {count} files')
