b18a84735c6ef5a300892f8fd1f0c7b81406a87d
Hunter Welch <hjw36@case.edu>
2023-01-07 14:32:48 -0500

    Updated Scrapper to work with facebook
    Read User entered Facebook username and password and enters that into facebook. Then redirects to the birthdays tab.

--- Scraper_Testing/scraper.py
+++ Scraper_Testing/scraper.py
@@ -1,8 +1,38 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+import pandas as pd
+
+# some code is utilized from kromme's Scrape-Facebook-birthdays' github repo
+
+username = input("Enter Facebook username: ")
+password = input("Enter Facebook password: ")
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+driver = webdriver.Chrome(PATH, options=options)
 
-driver.get("https://www.google.com")
-print(driver.title)
+driver.get("https://www.facebook.com/")
+
+# find username box and type user's username
+username_entry = driver.find_element(By.ID, 'email')
+username_entry.send_keys(username)
+
+# find password box and type user's password
+password_entry = driver.find_element(By.ID, 'pass')
+password_entry.send_keys(password)
+
+# hit enter to send the username and password to facebook
+password_entry.send_keys(Keys.RETURN)
+
+# wait 5 seconds to see if it worked
+time.sleep(5)
+
+# send the user to the birthday tab
+driver.get("https://www.facebook.com/events/birthdays/")
+time.sleep(5)
+
+
 driver.quit()
